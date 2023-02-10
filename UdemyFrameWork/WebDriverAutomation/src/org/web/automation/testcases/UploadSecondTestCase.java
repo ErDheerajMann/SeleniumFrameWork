580017b3cfd4c7c043e3c61f803b3da2abee2519
hp <hp@DESKTOP-6UKA0IR>
2023-02-10 11:41:19 +0530

    MySecondCommit

---
+++ UdemyFrameWork/WebDriverAutomation/src/org/web/automation/testcases/UploadSecondTestCase.java
@@ -0,0 +1,11 @@
+package org.web.automation.testcases;
+
+public class UploadSecondTestCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("SecondTestCase");
+
+	}
+
+}
